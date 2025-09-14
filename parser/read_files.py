2e893a976961e66035b9fadf1e00ddb8c778a2b2
Nurlan <nurlan.grachyov@mail.ru>
2025-09-14 21:16:10 +0400

    added file reading

--- parser/read_files.py
+++ parser/read_files.py
@@ -9,16 +9,17 @@ def read_file():
     for filename in os.listdir(data_dir):
         filepath = os.path.join(data_dir, filename)
         if os.path.isfile(filepath):
-            df = pd.read_excel(filepath)
-            df["Количество Договоров, шт."] = pd.to_numeric(df["Количество Договоров, шт."], errors='coerce').fillna(0)
-            count = df[df["Количество Договоров, шт."] > 0]
-            if count:
-                exchange_product_id = df["Код Инструмента"]
-                exchange_product_name = df["Наименование Инструмента"]
-                delivery_basis_name = df["Базис поставки"]
-                volume = df["Объем Договоров в единицах измерения"]
-                total = df["Объем Договоров, руб."]
-
+            df = pd.read_excel(filepath, skiprows=6)
+            df["Количество\nДоговоров,\nшт."] = pd.to_numeric(df["Количество\nДоговоров,\nшт."], errors='coerce').fillna(0)
+            filtered_df = df[df["Количество\nДоговоров,\nшт."] > 0]
+            if not filtered_df.empty:
+                exchange_product_id = filtered_df["Код\nИнструмента"]
+                exchange_product_name = filtered_df["Наименование\nИнструмента"]
+                delivery_basis_name = filtered_df["Базис\nпоставки"]
+                volume = filtered_df["Объем\nДоговоров\nв единицах\nизмерения"]
+                total = filtered_df["Обьем\nДоговоров,\nруб."]
+                count = filtered_df["Количество\nДоговоров,\nшт."]
+                print(count)
 
 
 read_file()
