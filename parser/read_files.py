1619beace34fc71afc62e082777627f2834b57cb
Nurlan <nurlan.grachyov@mail.ru>
2025-09-14 20:30:02 +0400

    added file reading

--- parser/read_files.py
+++ parser/read_files.py
@@ -1,4 +1,5 @@
 import os
+import pandas as pd
 
 current_dir = os.path.dirname(__file__)
 data_dir = os.path.join(current_dir, '..', 'data')
@@ -8,7 +9,16 @@ def read_file():
     for filename in os.listdir(data_dir):
         filepath = os.path.join(data_dir, filename)
         if os.path.isfile(filepath):
-            print("1")
+            df = pd.read_excel(filepath)
+            df["Количество Договоров, шт."] = pd.to_numeric(df["Количество Договоров, шт."], errors='coerce').fillna(0)
+            count = df[df["Количество Договоров, шт."] > 0]
+            if count:
+                exchange_product_id = df["Код Инструмента"]
+                exchange_product_name = df["Наименование Инструмента"]
+                delivery_basis_name = df["Базис поставки"]
+                volume = df["Объем Договоров в единицах измерения"]
+                total = df["Объем Договоров, руб."]
+
 
 
 read_file()
